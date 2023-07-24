100b25faa1aa2ee7e9346e47760de38998c367ed
Victor Matekole <victor@matekole.com>
2023-07-24 17:08:46 +0200

    Squashed commit of the following:
    commit aa3982103f1c165c13049aeacb178051379b5278
    Author: Victor Matekole <victor@matekole.com>
    Date:   Mon Jul 24 17:06:42 2023 +0200
    
        formatting
    
    commit 24ee980ddc47914f18ab5ffaf5f6de7d554a7b0f
    Author: Victor Matekole <victor@matekole.com>
    Date:   Mon Jul 24 13:50:57 2023 +0200
    
        formatting
    
    commit a3b8fc267143d7c447ac78a250412a7f02d32d63
    Author: Victor Matekole <victor@matekole.com>
    Date:   Mon Jul 24 13:17:33 2023 +0200
    
        update gh workflow and add data
    
    commit f69632c8f0ad5f0ab3860c641102ed52459ce85b
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 17:32:21 2023 +0200
    
        fix workflow
    
    commit c2729aaeb87e6c180d47b47a0e0e9c59b192addb
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 16:56:33 2023 +0200
    
        update training method to use dvc data
    
    commit 914a343aeee05c71d48f4759d269c78247e7d4ea
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 15:37:37 2023 +0200
    
        fix github action
    
    commit d4942c4e46d12ba588849c59c1bcdf8956cfd351
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 15:34:41 2023 +0200
    
        fix github action debug
    
    commit 6cb62c8fee691c930a30ef866a463ac7f449a29b
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 15:18:52 2023 +0200
    
        fix github action
    
    commit 64148a86bcbf6e992c69ff57e1f3acf5b59d3bb2
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 14:54:36 2023 +0200
    
        fix github action
    
    commit ab8d73bdb269bff10a1a414bd86167b8b0f48834
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 14:51:20 2023 +0200
    
        fix github action
    
    commit b2329822caa16a459c320d2493a7eb7e7cd8e59d
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 14:48:48 2023 +0200
    
        fix github action
    
    commit ba1a84416ec8436a0393eaa764301b026e70192b
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 14:43:02 2023 +0200
    
        fix github action
    
    commit 400eab394486cdc95beaa333a8a39be579c6a908
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 14:42:04 2023 +0200
    
        fix github action
    
    commit 667948c1c05ff39df9f4575deb009f3562b7b7f3
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 14:40:03 2023 +0200
    
        fix github action
    
    commit 167d5ea1659c1e82abab1bc68f7391b64a32bee1
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 14:35:58 2023 +0200
    
        update git workflow, reference data and add send_data.py
    
    commit b15778b730964f39717a6ada6afc063a1c45a6a4
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 13:43:20 2023 +0200
    
        add predict.py
    
    commit 5b730eb2a0f7d64502f8cb261112a9311b4e4589
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 13:27:49 2023 +0200
    
        update flow
    
    commit 979979b4988da436bca4f6ddb365ab19cee90156
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 13:25:50 2023 +0200
    
        update ports
    
    commit 3ef712a5f990ab5f382e4bc515be39f2663a46ab
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 13:19:57 2023 +0200
    
        update tag name
    
    commit 3a1fbd0d533117b2f266856cae4e8d5dfc6dcbf5
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 12:07:48 2023 +0200
    
        add git workflow
    
    commit 1b4925008f7aa033710a57380fa2442eaa959b8a
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 11:29:54 2023 +0200
    
        add git workflow
    
    commit 38eea9615d078a7c29c387a9fee8bf451b14a167
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 11:11:56 2023 +0200
    
        delete file
    
    commit f1cf2101ae6f3eb5209b69eef32f4f1bd5d6a925
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 11:02:42 2023 +0200
    
        add remote storage
    
    commit f9fb02a1411ba8c8f1f4512f832b95a39265bc94
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 10:53:32 2023 +0200
    
        add code and git workflows
    
    commit 1a2c71b7df6358dba605d1a97b14844500510091
    Author: Victor Matekole <victor@matekole.com>
    Date:   Fri Jul 21 10:52:45 2023 +0200
    
        add data

---
+++ model/train_deploy.py
@@ -0,0 +1,118 @@
+import argparse
+import os
+from dotenv import load_dotenv
+
+import pandas as pd
+
+import mlflow
+from mlflow.tracking.client import MlflowClient
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+from rich import print
+from rich.console import Console
+
+# year = 2021
+# month = 9
+# color = "green"
+
+# # Download the data
+# if not os.path.exists(f"./data/{color}_tripdata_{year}-{month:02d}.parquet"):
+#     os.system(
+#         f"wget -P ./data https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02d}.parquet")
+
+# Load the data
+
+df = pd.read_parquet(f"./data/training/dvc_train.parquet")
+
+load_dotenv()
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+
+# Set up the connection to MLflow
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+# Setup the MLflow experiment
+mlflow.set_experiment("green-taxi-monitoring-project")
+
+features = ["PULocationID", "DOLocationID", "trip_distance",
+            "passenger_count", "fare_amount", "total_amount"]
+target = 'duration'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--cml_run", default=False, action=argparse.BooleanOptionalAction, required=True
+)
+args = parser.parse_args()
+cml_run = args.cml_run
+
+# calculate the trip duration in minutes and drop trips that are less than 1 minute and more than 2 hours
+
+
+def calculate_trip_duration_in_minutes(df):
+    df["duration"] = (df["lpep_dropoff_datetime"] -
+                      df["lpep_pickup_datetime"]).dt.total_seconds() / 60
+    df = df[(df["duration"] >= 1) & (df["duration"] <= 60)]
+    df = df[(df['passenger_count'] > 0) & (df['passenger_count'] < 8)]
+    df = df[features + [target]]
+    return df
+
+
+def main():        
+    df_processed = calculate_trip_duration_in_minutes(df)
+
+    y = df_processed["duration"]
+    X = df_processed.drop(columns=["duration"])
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, random_state=42, test_size=0.2)
+
+    GOOGLE_APPLICATION_CREDENTIALS: str = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")    
+    if not GOOGLE_APPLICATION_CREDENTIALS:
+        print("Gettng SA_KEY")
+        SA_KEY = os.getenv("SA_KEY")
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = SA_KEY
+
+    with mlflow.start_run():
+        tags = {
+            "model": "linear regression",
+            "developer": "Victor Matekole",
+            "dataset": f"Defined in DVC/Git — Should provide the hash and details from DVC/Git",            
+            "features": features,
+            "target": target
+        }
+        mlflow.set_tags(tags)
+
+        lr = LinearRegression()
+        lr.fit(X_train, y_train)
+
+        y_pred = lr.predict(X_test)
+        rmse_test = mean_squared_error(y_test, y_pred, squared=False)
+        mlflow.log_metric("rmse", rmse_test)
+
+        mlflow.sklearn.log_model(lr, "model")
+        run_id = mlflow.active_run().info.run_id
+
+        model_uri = f"runs:/{run_id}/model"
+        model_name = "green-taxi-ride-duration-project"
+        mlflow.register_model(model_uri=model_uri, name=model_name)
+        models = client.get_latest_versions(model_name, stages=["None"])
+        model_version = models[0].version
+        new_stage = "Production"
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version,
+            stage=new_stage,
+            archive_existing_versions=True
+        )
+
+        if cml_run:
+            with open("metrics.txt", "w") as f:
+                f.write(f"RMSE on the Test Set: {rmse_test}")
+
+
+if __name__ == '__main__':
+    main()
